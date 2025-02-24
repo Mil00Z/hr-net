a460fe03efb0f904a26ae4aa2b0377e127a8f7df
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-24 18:11:58 +0100

    upDates Form logic and get datas from Store Redux with Selector

--- src/components/Forms/FormCreateEmployees.tsx
+++ src/components/Forms/FormCreateEmployees.tsx
@@ -1,64 +1,89 @@
 "use client";
 
 import {useState,useEffect} from 'react';
+import {useSelector,useDispatch} from 'react-redux';
+import {nanoid} from '@reduxjs/toolkit';
+
+import { employeesSlice } from '@/redux/employees/employesSlice';
 
 
 const FormCreateEmployees = () => {
 
+  const quickStore = useSelector((state) => state.user);
 
-  function saveEmployee(event){
 
-    event.preventDefault;
+  const dispatch = useDispatch();
 
-    console.log('click submit')
+
+
+  function saveEmployee(event) {
+    
+
+    event.preventDefault();
+
+    const formDatas = new FormData(event.target);
+
+    let copyDatas = Object.fromEntries(formDatas)
+
+    const employeeData = {...copyDatas,id:nanoid()}
+
+
+    //Update Store
+    dispatch(employeesSlice.actions.setEmployees([...quickStore.employees,employeeData]));
+
+  
+    //Check Update
+    // console.log(quickStore)
 
   }
 
   return(
     <>
-    <form action="/" id="create-employee">
+    <form action="/" id="create-employee" onSubmit={()=>{saveEmployee(event)}}>
 
-          <label htmlFor="first-name" className="input-label">First Name</label>
-          <input type="text" id="first-name"/>
+          <label htmlFor="firstName" className="input-label">First Name</label>
+          <input type="text" id="firstName" name="firstName"/>
 
-          <label htmlFor="last-name" className="input-label">Last Name</label>
-          <input type="text" id="last-name"/>
+          <label htmlFor="lastName" className="input-label">Last Name</label>
+          <input type="text" id="lastName" name="lastName"/>
 
-          <label htmlfor="date-of-birth" className="input-label">Date of Birth</label>
-          <input id="date-of-birth" type="date"/>
+          <label htmlFor="dateBirth" className="input-label">Date of Birth</label>
+          <input id="dateBirth" name="dateBirth" type="date"/>
 
           <label htmlFor="start-date" className="input-label">Start Date</label>
-          <input id="start-date" type="date"/>
+          <input id="startDate" name="startDate" type="date"/>
 
 
           <fieldset className="address">
                     <legend>Address</legend>
 
                     <label htmlFor="street" className="input-label">Street</label>
-                    <input id="street" type="text" />
+                    <input id="street" name="street" type="text" />
 
                     <label htmlFor="city" className="input-label">City</label>
-                    <input id="city" type="text" />
+                    <input id="city" name="city" type="text" />
 
                     <label htmlFor="state" className="input-label">State</label>
-                    <select name="state" id="state"></select>
+                    <select name="state" name="state" id="state"></select>
 
-                    <label htmlFor="zip-code" className="input-label">Zip Code</label>
-                    <input id="zip-code" type="number" />
+                    <label htmlFor="zipCode" className="input-label">Zip Code</label>
+                    <input id="zipCode" name="zipCode	" type="number" />
             </fieldset>
 
           <label htmlFor="department" className="input-label">Department</label>
-          <select name="department" id="department">
+          <select name="department" id="department" name="department">
                     <option>Sales</option>
                     <option>Marketing</option>
                     <option>Engineering</option>
                     <option>Human Resources</option>
                     <option>Legal</option>
           </select>
-                
+
+
+          <button className="btn" type="submit">Save</button>       
         </form>
 
-        <button className="btn" onClick={()=>{saveEmployee(event)}}>Save</button>
+        
 
       </>
   )
