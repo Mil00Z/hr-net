7cf5d2a5af1201fa46c8d2c25609a29ba26c47d0
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-25 15:11:54 +0100

    add useEffect and useState to display Warning message bottom of list

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
+import {useState,useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
 import Link from 'next/link'
 
 import DataTable from '@/components/DataTable/Datatable';
 
-
-
 // import '@/styles/pages/NotFound.scss'
 
 const EmployeesList = () => {
 
   const employeesDatas = useSelector((state) => state.user.employees)
+
+  const [datasAvailable,setdatasAvailable] = useState(null);
+
+
+  useEffect(() => { 
+
+    if(employeesDatas.length > 0){
+
+      setdatasAvailable(true);
+
+    }
+
+  }, [employeesDatas]);
 
 
   return (
@@ -27,7 +39,7 @@ const EmployeesList = () => {
 
       <DataTable drillingDatas={employeesDatas} /> 
 
-      {employeesDatas.length === 0 ? (<p>'No data available in table'</p>): null }
+      {!datasAvailable ? (<p>'No data available in store : Mock datas ON'</p>): (<p>Showing 1 of {employeesDatas.length} entries</p>) }
 
     </section> 
 
