01b742c4f19318c90ee482ece1a549302c1998dd
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-03-03 16:24:14 +0100

    refactor html render with Tailwind class

--- src/components/DataTable/Datatable.tsx
+++ src/components/DataTable/Datatable.tsx
@@ -44,7 +44,7 @@ const DataTable = ({drillingDatas}) => {
     let entry = parseInt(inputValue);
 
     setFilteredDatas((filteredDatas) => initialDatas.slice(0, entry));
-
+    
   }
 
 
@@ -77,7 +77,7 @@ const DataTable = ({drillingDatas}) => {
 
     } else {
 
-      const filteredSearch = filteredDatas.filter((row) =>{
+      const filteredSearch = filteredDatas.filter((row) => {
   
         return Object.values(row).some((value) =>{
           return value.toLowerCase().includes(input.toLowerCase());
@@ -92,8 +92,8 @@ const DataTable = ({drillingDatas}) => {
 
     useEffect(() => { 
 
-    //Activer un effet de mise à jour et visuel lorsque filteredDatas change ?
-    // console.log('datas changing');
+      //Activer un effet de mise à jour et visuel lorsque filteredDatas change ?
+      // console.log('datas changing');
 
   }, [filteredDatas]);
 
@@ -104,7 +104,7 @@ const DataTable = ({drillingDatas}) => {
 
         <div className="numbering flex flex-row">
 
-          <select className="" id="" onChange={(event)=>{entriesByPage(event.target.value)}}>
+          <select className="text-base text-white" onChange={(event)=>{entriesByPage(event.target.value)}}>
               <option defaultValue="" value={initialDatas.length}>All</option>
               <option value="1">1</option>
               <option value="4">4</option>
@@ -113,69 +113,64 @@ const DataTable = ({drillingDatas}) => {
               <option value="10">10</option>
               <option value="25">20</option>
           </select>
-          <label htmlFor="dt-length-0" className="px-4 text-white font-semibold"> entries per page</label>
+          <label htmlFor="dt-length-0" className="px-4 text-white font-semibold text-base"> entries per page</label>
           
         </div>
 
         <div className="searching flex flex-row">
-          <label htmlFor="search" className="px-2 mx-2 font-semibold text-white">Search:</label>
-          <input type="search" id="search" placeholder="votre recherche" onChange={(event)=>{globalSearch(event.target.value)}} />
+          <label htmlFor="search" className="px-2 mx-2 font-semibold text-white text-base">Search:</label>
+          <input type="search" id="search" className="text-base" placeholder="votre recherche" onChange={(event)=>{globalSearch(event.target.value)}} />
         </div>
         
       </div>
 
-      <table id="employee-table" className="data-table">
-          
-        <thead>
-          <tr>
+      
+      <table id="employee-table" className="table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr >
+                {colLabels.map((label,index) => {
+                return (<th key={`label-${index}`} className="px-6 py-3 text-yellow-600 text-lg" data-head={`${label}`} onClick={()=>{lexicalFilter(label)}} scope="col">{label}</th>)
+              })}
+              </tr>
+          </thead>
+          <tbody>
 
-          {colLabels.map((label,index) => {
-            return (<th key={`label-${index}`} className="table-label" data-head={`${label}`} onClick={()=>{lexicalFilter(label)}}>{label}</th>)
-          })}
-
-           </tr>
-        </thead>
-
-        
-        <tbody>
-          {filteredDatas.length > 0 ? (filteredDatas.map((row, index) => {
-              return (
-                <tr key={`id-${index}`} className="">
-                  {Object.values(row).map((cell, index) => {
-                    return (
-                      <td key={`cell-${index}`} className="table-cell">
-                        {cell.length !== 0 ? cell : '❌'}
-                      </td>
-                    );
-                  })}
+            {filteredDatas.length > 0 ? (filteredDatas.map((row, index) => {
+                  return (
+                    <tr key={`id-row-${index}`} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                      
+                      {Object.values(row).map((cell, index) => {
+                        return (
+                          <>
+                            <td key={`cell-${index}`} className="px-6 py-4 text-base">
+                            {cell.length !== 0 ? cell : '❌'}
+                            </td>
+                          </>
+                        );
+                      })}
+                      
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                  <>
+                    <td className="bg-red-600 px-6 py-4 text-white">
+                      Aucun résultat trouvé
+                    </td>
+                  </>
                 </tr>
-              );
-            })
-          ) : (
-            <tr>
-              <td className="table-cell-full">
-                Aucun résultat trouvé
-              </td>
-            </tr>
-          )}
-        </tbody>
+              )}
 
-        <tfoot>
-          <tr>
-
-          {colLabels.map((label,index) => {
-            return (<th key={`label-${index}`} className="table-label">{label}</th>)
-          })}
-
-           </tr>
-        </tfoot>
-
+          </tbody>
       </table>
 
-      <div className="bottom">
-        <div className="px-4 details text-white"> Show  <span className="font-semibold text-red-600">{filteredDatas.length}</span> entries of <span className="font-bold text-yellow-600">{initialDatas.length}</span> </div>
+
+      <div className="bottom my-2">
+        <div className="px-4 my-2 details text-white text-base"> Show <span className="text-lg font-semibold text-red-600">{filteredDatas.length}</span> entries of <span className="text-lg font-bold text-yellow-600">{initialDatas.length}</span></div>
         <div className="pagination"></div>
       </div>
+
     </>
 
   )
