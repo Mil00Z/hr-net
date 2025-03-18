48c4c0afedc13c2a780eb0f4859cbb0f36a13cbe
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-03-18 15:46:46 +0100

    add more context Types for ModalProps Component + add Closer Icon

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,7 +5,11 @@ import '@/styles/components/Modal.scss';
 export interface ModalProps {
   success: boolean;
   closeModal: Function;
-  newUser: object;
+  newUser: {
+    firstName: string;
+    lastName: string;
+    [key: string]: string;
+  };
 }
 
 const Modal = ({success,closeModal,newUser}: ModalProps) => {
@@ -19,8 +23,9 @@ function ModalClick() {
   return (
 
     <div className={`modal-success-container ${success ? 'show' : null}`}>
-      <div className="modal-content flex align-center justify-center" aria-labelledby="modal-title" role="dialog" aria-modal="true" onClick={(e) => ModalClick(e)}>
-            <p id="modal-title" className="title text-black-700 font-bold text-xl text-center">Add User <span className="text-green-600 mx-2"> {newUser.lastName} {newUser.firstName}</span></p>
+      <div className="modal-content flex align-center justify-center" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+          <p id="modal-title" className="title text-black-700 font-bold text-xl text-center">Added User : <span className="text-green-600 mx-2"> {newUser.lastName} {newUser.firstName}</span></p>
+          <span className="closer" onClick={(e) => ModalClick()}>❌</span>
       </div>
     </div>
 
