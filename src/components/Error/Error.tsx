95ebcc7a27c4dd11727104c266d25685d13b8e2f
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-03-21 13:29:44 +0100

    type Error Props

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,6 +1,9 @@
-const Error = (props) => {
 
-  const {error} = props;
+interface ErrorProps {
+  error:string
+}
+
+const Error = ({error} : ErrorProps) => {
 
   return (
     <h1>Something Went Wrong with Datas</h1>
