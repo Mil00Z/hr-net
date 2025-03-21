0c303ca2ebc3c7ce959b46227dfad66a22146df5
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-03-21 13:30:32 +0100

    Type Redux Slice and store

--- src/redux/employees/employesSlice.tsx
+++ src/redux/employees/employesSlice.tsx
@@ -1,14 +1,28 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type Employee = {
+  firstName: string;
+  lastName: string;
+  startDate: string;
+  department: string;
+  dateOfBirth: string;
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  id?: string;
+}
 
 const initialState = {
-  employees: [],
+  employees: [] as Employee[],
 };
+
 
 export const employeesSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setEmployees: (state, action) => {
+    setEmployees: (state, action: PayloadAction<Employee[]>) => {
       return {...state, employees: action.payload};
     },
   },
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -5,11 +5,11 @@ import {configureStore,combineReducers} from '@reduxjs/toolkit';
 import { employeesSlice } from '@/redux/employees/employesSlice';
 
 
-
-
 export const store = configureStore({
   preloadedState: {},
   reducer: combineReducers({
    user:employeesSlice.reducer
   })
 });
+
+export type RootState = ReturnType<typeof store.getState>;
