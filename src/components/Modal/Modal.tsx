d46a3d494e0ec498a5f89cfc9c8913a55e41faa1
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-03-24 17:45:59 +0100

    import type Employee to have one type in application , keep User type in backup

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,34 +1,37 @@
+import { Employee } from '@/redux/employees/employesSlice';
 
 import '@/styles/components/Modal.scss';
 
 
+// export type User = {
+//   firstName: string;
+//   lastName: string;
+//   id?: string;
+//   [key: string]: string | undefined; 
+// }
+
 export interface ModalProps {
   success: boolean;
-  closeModal: Function;
-  newUser: {
-    firstName: string;
-    lastName: string;
-    [key: string]: string;
-  };
+  closeModal: () => void;
+  newUser: Employee;
 }
 
-const Modal = ({success,closeModal,newUser}: ModalProps) => {
 
+const Modal = ({success,closeModal,newUser}: ModalProps) => {
 
 function ModalClick() {  
   closeModal();
 }
 
 
-  return (
+return (
 
     <div className={`modal-success-container ${success ? 'show' : null}`}>
       <div className="modal-content flex align-center justify-center" aria-labelledby="modal-title" role="dialog" aria-modal="true">
           <p id="modal-title" className="title text-black-700 font-bold text-xl text-center">Added User : <span className="text-green-600 mx-2"> {newUser.lastName} {newUser.firstName}</span></p>
-          <span className="closer" onClick={(e) => ModalClick()}>❌</span>
+          <span className="closer" onClick={() => ModalClick()}>❌</span>
       </div>
     </div>
-
   )
 }
 export default Modal
