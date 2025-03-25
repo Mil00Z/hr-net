75e1810b253ce8c634bc9fbe1089ffdacd188714
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-03-25 12:09:04 +0100

    clean, add hover button and check UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,26 @@
 import Link from "next/link";
 
 
-
 import FormCreateEmployees from '@/components/Forms/FormCreateEmployees';
+
 
 export default function Home() {
 
   return (
 
     <>
-
-        
-      <div className="container home">
+      <section className="container home">
 
         <h2 className="p-2 m-2 text-3xl text-center text-black-900">Create Employee</h2>
 
         <FormCreateEmployees />
 
-      </div>
+      </section>
 
-      <Link href="/employees" className="btn">Check Employees List</Link> 
+      <Link href="/employees" className="btn employees-vue font-semibold hover:bg-blue-900 hover:text-white">Check Employees List</Link> 
 
-    
     </>
 
   );
 }
+
